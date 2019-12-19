7d2584bb78a46afafcc6e22356e39edf7f9e28f4
Wolfgang Rönninger <wroennin@student.ethz.ch>
2019-12-19 21:14:18 +0100

    axi_llc: Add AXI last-level cache

--- scripts/compile_vsim.sh
+++ scripts/compile_vsim.sh
@@ -22,5 +22,6 @@ bender script vsim -t test -t rtl \
     --vlog-arg="-override_timescale 1ns/1ps" \
     --vlog-arg="-suppress 2583" \
     > compile.tcl
+
 echo 'return 0' >> compile.tcl
 $VSIM -c -do 'exit -code [source compile.tcl]'
